f76fd8f4e4ba8eaa4c707cc6127f69623da7683a
ohne <ghdlr785@gmail.com>
2022-08-23 17:56:48 +0900

    초기 커밋.

---
+++ cote55/cote55.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 전역변수 answer
+int answer = 0;
+
+void get_target_number(vector<int> numbers, int target, int sum, int index) 
+{
+    //종료 조건
+    if (index == numbers.size()) {
+        if (sum == target) {
+            answer++;
+        }
+        // 같지 않을때도 return
+        return;
+    }
+    //종료 조건이 만족되지않으면 계속 탐색
+    get_target_number(numbers, target, sum + numbers[index], index + 1);
+    get_target_number(numbers, target, sum - numbers[index], index + 1);
+
+
+}
+
+int solution(vector<int> numbers, int target) {
+    get_target_number(numbers, target, 0, 0);
+
+    return answer;
+}
+
+int main()
+{
+    vector<int> a = { 1,1,1,1,1 };
+    int target = 3;
+    solution(a, target);
+}
